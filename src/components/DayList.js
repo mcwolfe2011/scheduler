43893ef4fec0385ce32b2b0257a6b3f6327f8aa7
mcwolfe2011 <mcwolfe2011@gmail.com>
2023-04-19 07:51:32 -0700

    Fixed the bug. Move global variable into local scope.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,9 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
+
   const schedule = props.days.map(dayObj => {
+    
     return (
       <DayListItem
         key={dayObj.id}
