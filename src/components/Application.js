d50451c9c8c9fc386b31f073b989845c35b20ba1
mcwolfe2011 <mcwolfe2011@gmail.com>
2023-04-21 09:16:34 -0700

    Request api using axios, use proxy in package.json, transform data with selectors.

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import "components/Application.scss";
 import "components/Appointment";
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 
 const appointments = {
   1: {
@@ -45,25 +46,17 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({day: "Monday", days: []});
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days}));
 
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+  useEffect(() => {
+    Axios.get("api/days")
+    .then((days) => {
+      console.log('response', days);
+      setDays(days.data);
+    });
+  }, []);
 
   return (
     <main className="layout">
@@ -75,7 +68,10 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList 
+          days={state.days} 
+          value={state.day} 
+          onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
